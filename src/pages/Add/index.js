019098cee536e94903603ec8b8c22b473247de48
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-07-27 22:33:13 -0400

    CombineReducers, useDispath e refatoração

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addCar } from '../../store/cars';
+
+const INITIAL_STATE = {
+	name: '',
+	url: ''
+}
 
 export default function Add() {
+	const [form, setForm] = useState(INITIAL_STATE)
+
+	const dispath = useDispatch();
+
+
+	function handleChange(e)  {
+		const { name, value } = e.target;
+		setForm(form => ({ ...form, ...{ [name]: value } }));
+	};
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		dispath(addCar(form));
+		setForm(INITIAL_STATE)
+	}
+
 	return (
-		<form className="container mt-5">
+		<form className="container mt-5" onSubmit={handleSubmit}>
 			<div className="form-group">
 				<label>Nome</label>
-				<input type="text" name="name" className="form-control" placeholder="Nome..." />
+				<input onChange={handleChange} type="text" name="name" className="form-control" placeholder="Nome..." value={form.name} />
 			</div>
 			<div className="form-group">
 				<label>URL:</label>
-				<input type="text" name="url" className="form-control" placeholder="URL:https://cars" />
+				<input onChange={handleChange} type="text" name="url" className="form-control" placeholder="URL:https://cars" value={form.url} />
 			</div>
 			<button type="submit" className="btn btn-primary">
 				Adicionar
